59b8710f63a948edcfe75ee5c839acb4bb6fd90c
Anshu Kumar <anshu.builds@gmail.com>
2026-06-23 16:28:37 +0530

    feat: initialize project with database connection setup
    - created package.json with dependencies for dotenv, express, and mongoose
    - added .gitkeep file in public/temp directory
    - created app.js as the main application file
    - defined constants in constants.js
    - implemented database connection logic in src/db/index.js
    - set up entry point in src/index.js to connect to the database

---
+++ Day-6/src/db/index.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`,
+    );
+    console.log(
+      `MONGODB CONNECTION SUCCESSFULL !! ${connectionInstance.connection.host}`,
+    );
+  } catch (error) {
+    console.log(`MONGODB CONNECTION FAILED !!`, error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
