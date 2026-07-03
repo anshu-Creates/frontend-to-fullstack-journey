3f4f898a65eba59fb3bbced9b418e58130042e8f
Anshu Kumar <anshu.builds@gmail.com>
2026-07-03 17:10:06 +0530

    Add initial project setup with Express and EJS
    - Created package.json with dependencies for Express and EJS.
    - Added basic CSS styles for the application.
    - Created an index.ejs file for rendering the main HTML structure.
    - Added a JavaScript file for future client-side scripts.

---
+++ Day-11/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
